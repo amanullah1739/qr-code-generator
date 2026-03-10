586babf2730fe06300f8e3b73bd075c5f2e6bcba
amanullah1739 <amanullah1739@gmail.com>
2026-03-10 20:46:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+// const container = document.querySelector(".container"),
+//     image = document.querySelector(".QR-field img"),
+//     button = document.querySelector("button"),
+//     input = document.querySelector("input"),
+//     api = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+
+// button.addEventListener("click", () => {
+//     if(input.value.trim() !== "") {
+//         const qrURL = `${api}${encodeURIComponent(input.value)}`;
+//         image.src = qrURL;
+//         container.classList.add("active");
+//     } else {
+//         alert("Please enter text or URL");
+//     }
+// });
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker.register("/service-worker.js")
+//   .then(() => console.log("Service Worker Registered"));
+// }
+
+
 const container = document.querySelector(".container"),
-    image = document.querySelector(".QR-field img"),
-    button = document.querySelector("button"),
-    input = document.querySelector("input"),
-    api = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+image = document.querySelector(".QR-field img"),
+button = document.querySelector("button"),
+input = document.querySelector("input"),
+api = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
 
 button.addEventListener("click", () => {
     if(input.value.trim() !== "") {
@@ -13,7 +34,12 @@ button.addEventListener("click", () => {
         alert("Please enter text or URL");
     }
 });
+
+// Register Service Worker
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/service-worker.js")
-  .then(() => console.log("Service Worker Registered"));
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("service-worker.js")
+    .then(() => console.log("Service Worker Registered"))
+    .catch(err => console.log("Service Worker Error:", err));
+  });
 }
